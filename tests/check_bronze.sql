6cc597ba339f51e83e0fd86ea4055877130f0792
Oybek <alikulovoybek11@gmail.com>
2025-11-10 20:37:15 +0100

    Update and rename test.sql to check_bronze.sql

--- tests/check_bronze.sql
+++ tests/check_bronze.sql
@@ -26,13 +26,20 @@ FROM bronze.transactions_data
 GROUP BY id
 HAVING COUNT(*)>1 OR id IS NULL
 
-
+SELECT 
+	id,COUNT(*)
+FROM bronze.users_data
+GROUP BY id
+HAVING COUNT(*)>1 OR id IS NULL
 
 --Check for unwanted spaces
 --Expectation: No Result
 
 SELECT * FROM bronze.cards_data
 WHERE card_on_dark_web!=TRIM(card_on_dark_web)
+
+SELECT * FROM bronze.users_data
+WHERE gender!=TRIM(gender) or address!=TRIM(address)
 
 --Data standardization and Consistency
 
@@ -50,6 +57,8 @@ FROM bronze.transactions_data
 
 SELECT DISTINCT errors from bronze.transactions_data
 
+SELECT DISTINCT gender FROM bronze.users_data
+
 
 --Check for unexpected values
 --Expectation: NO Result
@@ -57,19 +66,28 @@ SELECT DISTINCT errors from bronze.transactions_data
 SELECT 
 	*
 FROM bronze.cards_data
-WHERE LEN(card_number) NOT IN(15,16) OR card_number NOT LIKE '%[0-9]%'
+WHERE LEN(card_number) NOT IN(15,16) OR card_number NOT LIKE '%[0-9]%' OR
+	  LEN(cvv) NOT IN(4,3) or cvv NOT LIKE '%[0-9]%'
 
 
 SELECT 
-	*
-FROM bronze.cards_data
-WHERE LEN(cvv) NOT IN(4,3) or cvv NOT LIKE '%[0-9]%'
+	* 
+FROM bronze.transactions_data
+WHERE client_id<0 OR card_id<0 OR merchant_id <0 OR mcc<0 OR 
+	  LEN(REPLACE(zip,'.0',''))<5
 
-SELECT * FROM bronze.transactions_data
-WHERE client_id<0 OR card_id<0 OR merchant_id <0 OR mcc<0
 
-SELECT * FROM bronze.transactions_data
-WHERE LEN(REPLACE(zip,'.0',''))<5
+SELECT 
+	* 
+FROM bronze.users_data
+WHERE current_age<0 OR 
+	  retirement_age<0 OR
+	  birth_year>GETDATE() OR 
+	  birth_month NOT BETWEEN 1 AND 12 OR
+	  latitude NOT BETWEEN -90 AND 90 OR
+	  longitude NOT BETWEEN -180 AND 180 OR
+	  credit_score<0 OR num_credit_cards<0
+
 
 --Check for unexpected date
 --Expectattion: No Result
@@ -77,17 +95,10 @@ WHERE LEN(REPLACE(zip,'.0',''))<5
 SELECT 
 *
 FROM bronze.cards_data
-WHERE expires NOT LIKE '[0-9][0-9]/[1-2][0-9][0-9][0-9]' OR expires IS NULL 
+WHERE expires NOT LIKE '[0-9][0-9]/[1-2][0-9][0-9][0-9]' OR expires IS NULL OR
+	  acct_open_date NOT LIKE '[0-9][0-9]/[1-2][0-9][0-9][0-9]' OR expires IS NULL OR
+	  year_pin_last_changed>GETDATE()
 
-SELECT 
-*
-FROM bronze.cards_data
-WHERE acct_open_date NOT LIKE '[0-9][0-9]/[1-2][0-9][0-9][0-9]' OR expires IS NULL 
-
-SELECT 
-	*
-FROM bronze.cards_data
-WHERE year_pin_last_changed>GETDATE()
 
 SELECT  * FROM bronze.transactions_data
 WHERE date>GETDATE()
